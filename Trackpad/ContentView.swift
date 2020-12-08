b958a5a1dee08b80e40be5048e7926db3c12b2b0
Joel  Giménez <joelgimenez@Joels-MacBook-Pro.local>
2020-12-08 14:09:04 -0300

    Initial Commit

---
+++ Trackpad/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Trackpad
+//
+//  Created by Joel  Giménez  on 2020-12-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Trackpad/TrackpadApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackpadApp.swift
+//  Trackpad
+//
+//  Created by Joel  Giménez  on 2020-12-08.
+//
+
+import SwiftUI
+
+@main
+struct TrackpadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
